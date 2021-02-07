f93ca1c06531327530a7b5b362f1bab1d2cd6c8e
Joseph Nagy <jn135@duke.edu>
2021-02-07 18:32:59 -0500

    implimented addGameDescriptions() and addGameInstructions

--- plotter/main.js
+++ plotter/main.js
@@ -5,15 +5,56 @@
  * @author Joseph Nagy
  */
 
+function addGameDescription (){
+    // game description header 
+    // create game description header element  
+    let header = document.createElement("h2"); 
+    let text = document.createTextNode("Game Description"); 
+    // append text to header element 
+    header.appendChild(text);
+    // append header element to document
+    document.getElementById("game-description-header").appendChild(header);
+
+    // game description content 
+    // create game description paragraph element  
+    let paragraph = document.createElement("p");
+    let content = document.createTextNode("THIS IS GAME DESCRIPTION CONTENT"); // add actual text later
+    // append text content to paragraph element 
+    paragraph.appendChild(content);
+    // append paragraph element to document
+    document.getElementById("game-description-content").appendChild(paragraph);
+}
+
+function addGameInstructions() {
+    // game instructions header 
+    // create game instructions header element  
+    let header = document.createElement("h2");
+    let text = document.createTextNode("How To Play");
+    // append text to header element 
+    header.appendChild(text);
+    // append header element to document
+    document.getElementById("instructions-header").appendChild(header);
+
+    // game instructions content 
+    // create game description paragraph element  
+    let paragraph = document.createElement("p");
+    let content = document.createTextNode("THIS IS GAME INSTRUCTIONS CONTENT"); // add actual text later
+    // append text content to paragraph element 
+    paragraph.appendChild(content);
+    // append paragraph element to document
+    document.getElementById("instructions-content").appendChild(paragraph);
+}
+
 function removeGameDescription (){ 
-     document.getElementById("game-description-header").remove();
-     document.getElementById("game-description-content").remove();
+    document.getElementById("game-description-header").querySelector("h2").remove()
+    document.getElementById("game-description-content").querySelector("p").remove()
  }
 
 function removeGameInstructions() {
-    document.getElementById("instructions-header").remove();
-    document.getElementById("instructions-content").remove();
+    document.getElementById("instructions-header").querySelector("h2").remove()
+    document.getElementById("instructions-content").querySelector("p").remove()
 }
+
 
 function startGame(){
     removeGameDescription(); 
