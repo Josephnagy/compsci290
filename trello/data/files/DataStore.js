7312cb4b7dcc2760d1fa79835785b0da7691e55f
Joseph Nagy <nagyjoseph9@gmail.com>
2021-03-18 18:00:42 -0400

    added edit card method to dataStore

--- trello/data/files/DataStore.js
+++ trello/data/files/DataStore.js
@@ -5,31 +5,34 @@
  */
 
  const trelloDataStore = {
-     searchText: "",
-     currentProject: EXAMPLE_PROJECT_0,
-     taskLists: EXAMPLE_PROJECT_0.taskLists,
-     cards: EXAMPLE_PROJECT_0.cards,
-     createNewTaskList: false,
-     cardToMoveDestination: null,
-     newCard: {
-         name: "",
-         color: "",
-         description: "",
-         deadline: "",
-         priority: "",
-         tags: [],
-         comments: [],
-         checklists: [],
-         showModal: false
-     },
-     newTaskList: {
-         name: "",
-         watch: false,
-         color: "",
-         cards: [],
-         cardOrderStyle: "alphabetical",
-         showModal: false,
-         createNewCard: false
-     },
-     newComment: ""
+     data: {
+         searchText: "",
+         currentProject: EXAMPLE_PROJECT_0,
+         createNewTaskList: false,
+         cardToMoveDestination: null,
+         newCard: {
+             name: "",
+             color: "",
+             description: "",
+             deadline: "",
+             priority: "",
+             tags: [],
+             comments: [],
+             checklists: [],
+             showModal: false
+         },
+         newTaskList: {
+             name: "",
+             watch: false,
+             color: "",
+             cards: [],
+             cardOrderStyle: "alphabetical",
+             showModal: false,
+             createNewCard: false
+         },
+         newComment: ""
+     }, 
+     updateCard(cardId, taskListId, editedCard){
+         this.data.currentProject.taskLists[taskListId].cards[cardId] = editedCard;
+     }
  }
