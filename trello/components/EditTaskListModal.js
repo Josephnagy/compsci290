c2b3c50612a73ae09abe32a8f47d7c1db758e1a1
Joseph Nagy <nagyjoseph9@gmail.com>
2021-03-17 20:24:37 -0400

    added more options to edit taskList

--- trello/components/EditTaskListModal.js
+++ trello/components/EditTaskListModal.js
@@ -25,6 +25,36 @@ Vue.component('edit-tasklist-modal', {
     },
 
     template: `
-        <p>hello world</p>
+    <div> 
+         <!-- taskList title  -->
+        <label><b>List Title</b></label>
+        <input v-model="currentTaskList.name" style="width: 100%" type="text"></input>
+        <hr />
+
+        <!-- taskList color  -->
+        <label><b>List Color</b></label>
+        <input v-model="currentTaskList.color" style="width: 100%" type="color"></input>
+        <hr />
+
+        <!-- options to sort cards within a list -->
+        <label><b>Card Ordering</b></label>
+        <select name="cardOrder" id="'editCardOrder'+taskListId" v-model="currentTaskList.cardOrderStyle">
+            <option value="alphabetical">Order alphabetically</option>
+            <option value="priority">Order by card priority</option>
+            <option value="deadline">Order by card deadline</option>
+        </select>
+
+        <!-- TODO: implement duplicate list button -->
+        <b-button 
+            variant="dark" 
+            block 
+            >Duplciate List</b-button>
+
+        <!-- TODO: implement delete taskList button -->
+        <b-button 
+            variant="danger" 
+            block 
+            >Delete List</b-button>
+    </div>
     `
 });
