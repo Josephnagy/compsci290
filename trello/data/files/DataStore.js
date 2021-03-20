13566040866fb38ce8bc6e4893285cd445fc08c0
Joseph Nagy <nagyjoseph9@gmail.com>
2021-03-20 19:00:31 -0400

    moved most methods to dataStore

--- trello/data/files/DataStore.js
+++ trello/data/files/DataStore.js
@@ -33,9 +33,112 @@
          newComment: ""
      }, 
 
+
     // ====================================================================================================================================
     // HELPER FUNCTIONS
     // ====================================================================================================================================
+
+     // takes taskList index and sortingCriterion, returns taskList[t].cards sorted based on sortingCriterion
+     sortCards(t, sortingCriterion) {
+         sortedCards = this.taskLists[t].cards;
+         // sort alpbabetically
+         if (sortingCriterion === "alphabetical") {
+             sortedCards.sort(function (a, b) {
+                 let textA = a.name.toUpperCase();
+                 let textB = b.name.toUpperCase();
+                 return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+             });
+             // sort by priority (high priority first, low priority last)
+         } else if (sortingCriterion === "priority") {
+             sortedCards.sort(function (a, b) {
+                 let priorityA = parseInt(a.priority);
+                 let priorityB = parseInt(b.priority);
+                 return priorityA - priorityB;
+             });
+             // sort by deadline (earliest deadlines first)
+         } else if (sortingCriterion === "deadline") {
+             sortedCards.sort(function (a, b) {
+                 // get current time 
+                 let now = new Date();
+                 // convert to ISOString 
+                 now = app.javascriptDateObjectToISOString(now); // had to use app instead of this 
+                 let hoursUntilDueA = this.hoursBetweenDates(now, a.deadline);
+                 let hoursUntilDueB = this.hoursBetweenDates(now, b.deadline)
+                 return hoursUntilDueA - hoursUntilDueB;
+             });
+         }
+     },
+
+    // ============================================  Date Related Functions  ==============================================================
+     
+    // helper function that takes 2 dates (as 'YYYY-MM-DDThh:mm') returns difference in hours 
+     hoursBetweenDates(date1, date2) {
+         d1 = Date.parse(date1);
+         d2 = Date.parse(date2);
+         return (d2 - d1) / (60 * 60 * 1000);
+     },
+
+     // helper function that converts military time to standard (ie 14:00 --> 2:00 PM)
+     militaryToStandardTime(militaryTime) {
+         // add blank seconds field to military time (necessary to use Moment.js)
+
+         // convert: moment(militaryTime, 'HH:mm:ss').format('h:mm:ss A');
+
+         // remove seconds field 
+
+         //return
+     },
+
+     // helper function that takes ISO date('YYYY-MM-DDThh:mm') and returns a simple display string 
+     displayDate(date) {
+         if (date === undefined) {
+             return "undefined time"
+         } else {
+             dateComponents = date.split('-');
+             year = dateComponents[0];
+             month = dateComponents[1];
+             day = dateComponents[2].substring(0, 2);
+             time = dateComponents[2].substring(3);
+             d = [month, day, year].join("/")
+             return d + " @ " + time;
+         }
+     },
+
+     // helper function that convert javascript date object to ISO string ('YYYY-MM-DDThh:mm')
+     javascriptDateObjectToISOString(date) {
+         day = date.getDate().toString();
+         day = ((day.length === 1) ? '0' + day : day); // add zero in front if necessary 
+         month = (date.getMonth() + 1).toString(); // +1 because .getMonth returns int 0-11
+         month = ((month.length === 1) ? '0' + month : month); // add zero in front if necessary
+         year = date.getFullYear().toString();
+         ymd = [year, month, day].join("-") // ymd = 'YYYY-MM-DD'
+         time = date.toString().split(" ")[4].substring(0, 5); // 'hh:mm'
+         return [ymd, time].join("T");
+     },
+
+    // ============================================  Style/Display Related Functions  =====================================================
+
+     displayPriority(priority) {
+         priorityDict = { 1: "Top Priority", 2: "Medium Priority", 3: "Low Priority" };
+         return priorityDict[priority];
+     },
+
+     getDeadlineClass(date) {
+         let currentTime = new Date();
+         hoursUntilDue = this.hoursBetweenDates(this.javascriptDateObjectToISOString(currentTime), date); // if hoursUntilDue < 0 => past due! 
+         // past due 
+         if (hoursUntilDue < 0) {
+             return 'overdue';
+         }
+         // due within 3 days 
+         else if (0 < hoursUntilDue && hoursUntilDue <= 72) {
+             return 'due-soon'
+         }
+         // due in the future
+         else {
+             return 'due-later'
+         }
+     },
 
     // ====================================================================================================================================
     // LIST METHODS
--- trello/main.js
+++ trello/main.js
@@ -23,104 +23,11 @@ let backgroundImageOptions = ['beach.jpg', 'city.jpg', 'mountains.jpg', 'mushroo
  const app = new Vue({
      data(){
          return {
+             // primary data store 
              allData: trelloDataStore
          }
      }, 
      methods: {
-         // helper function that converts military time to standard (ie 14:00 --> 2:00 PM)
-         militaryToStandardTime(militaryTime){
-             // add blank seconds field to military time (necessary to use Moment.js)
-
-             // convert: moment(militaryTime, 'HH:mm:ss').format('h:mm:ss A');
-
-             // remove seconds field 
-
-             //return
-
-         },
-         // helper function that takes ISO date('YYYY-MM-DDThh:mm') and returns a simple display string 
-         displayDate(date){
-             if(date === undefined){
-                 return "undefined time"
-             } else {
-                 dateComponents = date.split('-');
-                 year = dateComponents[0];
-                 month = dateComponents[1];
-                 day = dateComponents[2].substring(0, 2);
-                 time = dateComponents[2].substring(3);
-                 d = [month, day, year].join("/")
-                 return d + " @ " + time;
-             }
-         }, 
-         displayPriority(priority){
-             priorityDict = {1: "Top Priority", 2: "Medium Priority", 3: "Low Priority"}; 
-             return priorityDict[priority]; 
-         }, 
-         // helper function that convert javascript date object to ISO string ('YYYY-MM-DDThh:mm')
-         javascriptDateObjectToISOString(date){
-             day = date.getDate().toString(); 
-             day = ( (day.length === 1) ? '0' + day : day); // add zero in front if necessary 
-             month = (date.getMonth()+1).toString(); // +1 because .getMonth returns int 0-11
-             month = ((month.length === 1) ? '0' + month : month); // add zero in front if necessary
-             year = date.getFullYear().toString();
-             ymd = [year, month, day].join("-") // ymd = 'YYYY-MM-DD'
-             time = date.toString().split(" ")[4].substring(0, 5); // 'hh:mm'
-             return [ymd, time].join("T");
-         },
-         getDeadlineClass(date){
-            let currentTime = new Date(); 
-             hoursUntilDue = this.hoursBetweenDates(this.javascriptDateObjectToISOString(currentTime), date); // if hoursUntilDue < 0 => past due! 
-             // past due 
-             if(hoursUntilDue < 0 ){
-                return 'overdue';
-             } 
-            // due within 3 days 
-             else if (0 < hoursUntilDue && hoursUntilDue <= 72) {
-                return 'due-soon'
-             }
-             // due in the future
-             else {
-                 return 'due-later'
-             }
-         },
-         // helper function that takes 2 dates (as 'YYYY-MM-DDThh:mm') returns difference in hours 
-         hoursBetweenDates(date1, date2){
-             d1 = Date.parse(date1);
-             d2 = Date.parse(date2); 
-             return (d2 - d1) / (60 * 60 * 1000); 
-         }, 
-         // takes taskList index and sortingCriterion, returns taskList[t].cards sorted based on sortingCriterion
-         sortCards(t, sortingCriterion){
-             sortedCards = this.taskLists[t].cards;
-             // sort alpbabetically
-             if (sortingCriterion === "alphabetical"){
-                 sortedCards.sort(function (a, b) {
-                     let textA = a.name.toUpperCase();
-                     let textB = b.name.toUpperCase();
-                     return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
-                 });
-            // sort by priority (high priority first, low priority last)
-             } else if (sortingCriterion === "priority"){
-                 sortedCards.sort(function (a, b) { 
-                     let priorityA = parseInt(a.priority);
-                     let priorityB = parseInt(b.priority);
-                     return priorityA - priorityB;
-                 });
-            // sort by deadline (earliest deadlines first)
-             } else if (sortingCriterion === "deadline"){
-                 sortedCards.sort(function (a, b) {
-                     // get current time 
-                     let now = new Date();
-                     // convert to ISOString 
-                     now = app.javascriptDateObjectToISOString(now); // had to use app instead of this 
-                     let hoursUntilDueA = app.hoursBetweenDates(now, a.deadline);
-                     let hoursUntilDueB = app.hoursBetweenDates(now, b.deadline)
-                     return hoursUntilDueA - hoursUntilDueB;
-                 });
-             }
-             // save sorted cards 
-             this.taskLists[t].cards = sortedCards;
-         },
          // moves card from one taskList to another 
          moveCard(t, c){
              // store card object 
@@ -194,24 +101,11 @@ let backgroundImageOptions = ['beach.jpg', 'city.jpg', 'mountains.jpg', 'mushroo
              this.newComment = "";
          }
      }, 
-     watch: {
-         // TODO
+     computed: {
      },
      template: 
      `
      <div> 
-        <!-- Page Header -->
-        <header>
-            <h1>{{allData.data.currentProject.name}}</h1>
-            <!-- EDIT BOARD -->
-            <!-- modal button -->
-            <b-button v-b-modal="'projectModal'+allData.data.currentProject.name" variant="dark" block>Edit Project</b-button>
-            <!-- actual modal to edit board  -->
-            <b-modal :id="'projectModal'+allData.data.currentProject.name" :title="allData.data.currentProject.name" :hide-footer="true">
-                <edit-project-modal :project="allData.data.currentProject"> </edit-project-modal>
-            </b-modal>
-
-        </header>
         <div >
             <main>
                 <project-board
